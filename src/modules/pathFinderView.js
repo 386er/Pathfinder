ddf3a11e5229ae948ec8671efec7e813811ed095
Tobias <tobi@mail.de>
2014-09-24 16:57:30 +0200

    Add connection lines

--- src/modules/pathFinderView.js
+++ src/modules/pathFinderView.js
@@ -34,7 +34,6 @@ define([
 			this._drawSegmentBorders();
 			this._addToolTip();
 
-
 			return this;
 		},
 
@@ -53,9 +52,7 @@ define([
 			for (
 				var i = 1; i <= NUMBER_SEGMENTS; i++) {
 				this.svg.append('line')
-					.attr('stroke', 'grey')
-					.attr('stroke-width', '1')
-					.attr('fill', 'red')
+					.attr('class', 'segmentline')
 					.attr('x1', segmentWidth * i)
 					.attr('y1', margin.top)
 					.attr('x2', segmentWidth * i)
@@ -89,40 +86,84 @@ define([
 		},
 
 		_prerenderData: function() {
-			var chart = this;
-
-			var yScale = this._getYScale(),
+			var chart = this,
+			yScale = this._getYScale(),
 			segmentWidth = (this.width / 7),
-			halfsegmentwidth = (this.width / 14);
+			halfSegmentWidth = (this.width / 14);
+			chart.idRegister = {};
+
 
 			chart.collection.each(
 				function(model) {
 					/*jshint sub:true*/
 					var segmentType = chart._getSegmentType(
 						model.attributes['group_type']);
-					/*jshint sub:false*/
+
 					model.attributes.x =
-						halfsegmentwidth + segmentWidth *
+						halfSegmentWidth + segmentWidth *
 						segmentType + chart._ranNum();
 
 					var clusterTimeStamp = chart._getClusterTime(model.attributes);
 					var clusterDate = moment.unix(clusterTimeStamp).toDate();
+
 					model.attributes.y = yScale(clusterDate);
 
 					var count = model.attributes.count;
 					model.attributes.r = 5 + count;
-				}
+
+					chart.idRegister[
+						model.attributes['cluster_id']] = {};
+					chart.idRegister[
+						model.attributes['cluster_id']].x = model.attributes.x;
+					chart.idRegister[
+						model.attributes['cluster_id']].y = model.attributes.y;
+
+				}	/*jshint sub:false*/
 			);
 		},
 
 		_renderData: function() {
 			var chart = this;
 
+			chart.collection.models.forEach(
+					/*jshint sub:true*/
+				function(model) {
+					var outlinks = model.attributes['out_links'];
+					if (outlinks.length) {
+						outlinks.forEach(
+							function(item) {
+								var inID = item['in_cluster_id'];
+					/*jshint sub:false*/
+								if (chart.idRegister[inID]) {
+
+									var outX = model.attributes.x,
+									outY = model.attributes.y,
+									inX = chart.idRegister[inID].x,
+									inY = chart.idRegister[inID].y;
+
+									chart.svg.append('line')
+										.attr('class', 'connectionline')
+										.attr('x1', outX)
+										.attr('y1', outY)
+										.attr('x2', inX)
+										.attr('y2', inY);
+								}
+							}
+						);
+					}
+				}
+			);
+
 			chart.svg.selectAll('circle')
 				.data(chart.collection.models)
 				.enter()
 				.append('circle')
 				.attr('class', 'mention')
+				.attr('id', function(model) {
+					/*jshint sub:true*/
+					return model.attributes['cluster_id'];
+					/*jshint sub:false*/
+				})
 				.attr('cx', function(model) {
 					return model.attributes.x;
 				})
@@ -134,29 +175,43 @@ define([
 				})
 				.on('mouseover', function(model) {
 
-					d3.select(this).transition().duration(200)
-						.style('opacity', 0.5)
-						.style('stroke', 'orange');
+					d3.select(this)
+						.style('stroke-width', 3);
 
 					chart.toolTip.model = model;
 					chart.toolTip.render();
 					chart.toolTip.move(
-						(d3.mouse(this)[0] + 55),
+						(d3.mouse(this)[0] - 20 ),
 						(d3.mouse(this)[1] + 45));
 					chart.toolTip.show();
+
+					chart.svg.append('line')
+						.attr( 'class', 'markerline')
+						.attr('x1', this.cx.baseVal.value)
+						.attr('y1', chart.defaults.margin.top)
+						.attr('x2', this.cx.baseVal.value)
+						.attr('y2', this.cy.baseVal.value - this.r.baseVal.value);
+
+					chart.svg.append('line')
+						.attr( 'class', 'markerline')
+						.attr('x1', this.cx.baseVal.value)
+						.attr('y1', this.cy.baseVal.value + this.r.baseVal.value)
+						.attr('x2', this.cx.baseVal.value)
+						.attr('y2', chart.height);
 
 				})
 				.on('mousemove', function() {
 					chart.toolTip.move(
-						(d3.mouse(this)[0] + 55),
+						(d3.mouse(this)[0] - 20 ),
 						(d3.mouse(this)[1] + 45));
 				})
 				.on('mouseout', function() {
-					d3.select(this).transition().duration(200)
-						.style('opacity', 1)
-						.style('stroke', '#EFF5FB');
+					d3.select(this)
+						.style('stroke-width', 1);
 
 					chart.toolTip.hide();
+
+					chart.svg.selectAll('line.markerline').remove();
 
 				});
 
